fb45c36d42ed52018b944a07b88c8f0baee7d69b
Roman Globa <globaroman@gmail.com>
2022-10-07 15:53:16 +0300

    implemented class NewComber and method showInf into class

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,12 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        NewComber.showInf("Hello Java and Git");
     }
+}
+class NewComber{
+    static void showInf(String s){
+        System.out.println(s);
+    }
+
 }
