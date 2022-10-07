4d3327b482808169a38284a381329399cc5bcbe6
Roman Globa <globaroman@gmail.com>
2022-10-07 15:59:13 +0300

    added 1 row I will not be here

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         NewComber.showInf("Hello Java and Git");
+        NewComber.showInf("I will not be here");
     }
 }
 class NewComber{
